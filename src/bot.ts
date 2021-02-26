9ec5f3b306a0d51bb9fe88061608f87e28f1d063
The God <79669205+TheGod0001@users.noreply.github.com>
2021-02-26 20:12:15 +0100

    added auto react from whitelist and enforced style guide to bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,31 +1,35 @@
-import {Message} from 'discord.js';
-import {CommandoClient} from 'discord.js-commando';
-import {CONFIG} from './global';
+import { Message } from 'discord.js';
+import { CommandoClient } from 'discord.js-commando';
+import { CONFIG } from './global';
 
-class Bot extends CommandoClient{
-    
-    constructor() {
-        super({
-            commandPrefix: CONFIG.prefix
-        });
-        
+class Bot extends CommandoClient {
+  constructor() {
+    super({
+      commandPrefix: CONFIG.prefix,
+    });
+  }
+
+  public async start() {
+    this.registry
+      .registerDefaultTypes({
+        textChannel: true,
+      })
+      .registerGroup('staff')
+      .registerCommandsIn(`${__dirname}/commands`);
+    this.on('message', this.onMessage.bind(this));
+    await this.login(CONFIG.token);
+  }
+
+  public async onMessage(msg: Message) {
+    // to react to all commands
+    if (CONFIG.whitelist.includes(msg.channel.id)) {
+      const tasks = [];
+      for (let i = 0; i < CONFIG.emojis.length; i += 1) {
+        tasks.push(msg.react(CONFIG.emojis[i]));
+      }
+      await Promise.all(tasks);
     }
-    public async start(){
-        this.registry
-        .registerDefaultTypes({
-            textChannel: true
-        })
-        .registerGroup('staff')
-        .registerCommandsIn(`${__dirname}/commands`);
-        this.on('message',this.onMessage.bind(this));
-        await this.login(CONFIG.token);
-    }
-    public async onMessage(msg: Message){
-        if(CONFIG.whitelist.includes(msg.channel.id)){
-            await msg.react('✔');
-            await msg.react('😊');
-        }
-    }
+  }
 }
 
 const bot = new Bot();
